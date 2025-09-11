525e53e7c4e92229b94cd8dc943bb211672ef1b9
hswhouse <hswhouse@gmail.com>
2025-09-11 15:02:17 +0900

    CRM-46 [feat] 플레이어 기본 틀 제작

---
+++ Outcry/Assets/02. Scripts/Player/PlayerMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+
+    }
+
+}
