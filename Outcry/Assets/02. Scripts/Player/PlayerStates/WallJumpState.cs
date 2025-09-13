8b935987d0ee999226c6daf02124720b81688af2
HyerimeLee <hyerimLee4426@gmail.com>
2025-09-14 00:18:18 +0900

    CRM-49 [refactor] 좌우 벽 번갈아 점프 되는데 아직 같은벽 또 점프됨.

--- Outcry/Assets/02. Scripts/Player/PlayerStates/WallJumpState.cs
+++ Outcry/Assets/02. Scripts/Player/PlayerStates/WallJumpState.cs
@@ -13,11 +13,22 @@ public class WallJumpState : IPlayerState
 
     public void HandleInput(PlayerController player) 
     {
+        //if (player.Inputs.Player.Move.ReadValue<Vector2>().x != 0)
+        //{
+        //    player.PlayerMove.Move();
+        //    return;
+        //}
 
-        if(player.PlayerMove.isWallTouched && player.PlayerMove.prevWall != player.PlayerMove.curWall)
+        if (player.PlayerMove.isWallTouched && player.PlayerMove.prevWall != player.PlayerMove.curWall)
         {
 
             player.ChangeState<WallHoldState>();
+            return;
+        }
+
+        if(player.Inputs.Player.Jump.triggered && !player.PlayerMove.isDoubleJump)
+        {
+            player.ChangeState<DoubleJumpState>();
             return;
         }
 
@@ -25,11 +36,7 @@ public class WallJumpState : IPlayerState
 
     public void LogicUpdate(PlayerController player)
     {
-        //if (player.Inputs.Player.Move.ReadValue<Vector2>().x != 0)
-        //{
-        //    player.PlayerMove.Move();
-        //    return;
-        //}
+        
         if (player.PlayerMove.rb.velocity.y < 0)
         {
             player.ChangeState<FallState>();
