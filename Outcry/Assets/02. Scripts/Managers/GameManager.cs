fc0791d63e2c177c5cc778fdc04fc97ab7321a33
Juwon Cha <littlebigcha@gmail.com>
2025-09-14 20:57:57 +0900

    CRM-59 [feat] 배열 데이터 변환 추가

--- Outcry/Assets/02. Scripts/Managers/GameManager.cs
+++ Outcry/Assets/02. Scripts/Managers/GameManager.cs
@@ -9,21 +9,6 @@ public class GameManager : Singleton<GameManager>
 {
     private void Start()
     {
-        var testPrefab = ResourceManager.Instance.LoadAssetAddressable<GameObject>("JoannaStatusUI/TestPrefab.prefab");
 
-        if(testPrefab != null)
-        {
-            GameObject prefab = Instantiate(testPrefab, Vector3.zero, Quaternion.identity);
-            StartCoroutine(DestroyTestCoroutine(prefab));
-        }
-    }
-
-    private IEnumerator DestroyTestCoroutine(GameObject prefab)
-    {
-        yield return new WaitForSeconds(5f);
-
-        Destroy(prefab);
-
-        ResourceManager.Instance.ReleaseAddressableAsset("JoannaStatusUI/TestPrefab.prefab");
     }
 }
