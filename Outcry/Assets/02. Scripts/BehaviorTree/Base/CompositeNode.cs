557304a69622258158449e790b332cc16f57f10c
Younga <youngajin93@gmail.com>
2025-09-14 13:57:59 +0900

    CRM-68 [refactor] 최최최최종

--- Outcry/Assets/02. Scripts/BehaviorTree/Base/CompositeNode.cs
+++ Outcry/Assets/02. Scripts/BehaviorTree/Base/CompositeNode.cs
@@ -10,10 +10,41 @@ public abstract class CompositeNode : Node
     public CompositeNode(List<Node> children = null)
     {
         this.children = children ?? new List<Node>();
+        this.currentIndex = 0;
+    }
+
+    public override NodeState Tick()
+    {
+        for (; currentIndex < children.Count; currentIndex++)
+        {
+            NodeState state = children[currentIndex].Tick();
+            
+            if (state == NodeState.Running)
+            {
+                return NodeState.Running;
+            }
+
+            if (state == NodeState.Failure)
+            {
+                currentIndex = 0;
+                return NodeState.Failure;
+            }
+        }
+        currentIndex = 0;
+        return NodeState.Success;
     }
 
     public virtual void AddChild(Node node)
     {
         children.Add(node);
+    }
+    
+    public override void Reset()
+    {
+        currentIndex = 0;
+        foreach (var child in children)
+        {
+            child.Reset();
+        }
     }
 }
