bc7da4ffbce0daaebf2188e57e832cf1033fac75
Younga <youngajin93@gmail.com>
2025-09-16 22:13:07 +0900

    CRM-47 [feat] TableDataHandler 구현
    세부적인것들은 기획테이블 데이터 준비되면 추가할것.

--- Outcry/Assets/02. Scripts/Data/TableDataHandler.cs
+++ Outcry/Assets/02. Scripts/Data/TableDataHandler.cs
@@ -1,6 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#region 임시
+
+using System;
+
+public class MonsterTableData{ //테이블에서 받아온 데이터. (가공전: 추후 기획테이블에서 자동생성될 예정)
+    public int monsterId;
+    public string monsterName;
+    public int health;
+    public float chaseSpeed;
+    public float attackRange;
+    public float attackCooldown;
+    public int[] specialSkillIds;
+    public int[] commonSkillIds;
+}
+
+#endregion
 
 /// <summary>
 /// 순수 변환만을 담당함 (기획테이블 데이터 -> 모델)
@@ -11,4 +27,22 @@ public static class TableDataHandler
 {
     // -GetExampleData(): ExampleModel static 
     // +GetExampleDatabase(): List<ExampleModel> static
+    public static BossMonsterModel MapFromTableData(MonsterTableData tableData)
+    {   
+        // deep copy 이유: 해당 모델 인스펙터에서 보여주기 위해 public 인자를 갖고 있음.
+        // 데이터 변경이 되더라도 해당 객체의 데이터만 변경될 것임.
+        int[] specialSkillIds = new int[tableData.specialSkillIds.Length];
+        Array.Copy(tableData.specialSkillIds, specialSkillIds, specialSkillIds.Length);
+        int[] commonSkillIds = new int[tableData.commonSkillIds.Length];
+        Array.Copy(tableData.commonSkillIds, commonSkillIds, commonSkillIds.Length);
+        
+        BossMonsterModel newBossMonsterModel = new BossMonsterModel(
+            tableData.monsterId, tableData.monsterName, tableData.health,
+            tableData.chaseSpeed, tableData.attackRange, tableData.attackCooldown,
+            specialSkillIds,
+            commonSkillIds);
+
+        return newBossMonsterModel;
+    }
+    //todo. 몬스터스킬데이터를 기획테이블에서 가져와서 맵핑하는 메서드 추가하기.
 }
