96ba1da518ffa787508cf1df5de454aba23ae2e1
Younga <youngajin93@gmail.com>
2025-09-17 14:44:47 +0900

    CRM-47 [chore] 기즈모 수정. (detectRange), 몬스터 히트박스 collider disable 해놓음.

--- Outcry/Assets/02. Scripts/Monsters/Base/MonsterBase.cs
+++ Outcry/Assets/02. Scripts/Monsters/Base/MonsterBase.cs
@@ -89,19 +89,41 @@ public abstract class MonsterBase : MonoBehaviour
 #if UNITY_EDITOR
     void OnDrawGizmos() //히트박스 색상 변경
     {
+        // 공격 범위 표시
         Gizmos.color = Color.red;
-
         if (hitbox != null && hitbox.enabled)
         {
             Gizmos.DrawWireCube(transform.position + (Vector3)hitbox.offset, hitbox.size);
         }
         
+        // 피격 범위 표시
         Gizmos.color = Color.yellow;
         BoxCollider2D hurtBox = GetComponent<BoxCollider2D>();
         if (hurtBox != null)
         {
             Gizmos.DrawWireCube(transform.position + (Vector3)hurtBox.offset, hurtBox.size);
         }
+        
+        //접근 범위 표시
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, monsterData.detectRange);
+        
+        //빨주노초파남보로 1, 2, 3, 4, 5, 6, 7 공격 범위 표시
+        // Gizmos.color = Color.red;
+        // Gizmos.DrawWireSphere(transform.position, 1f);
+        // Gizmos.color = new Color(1f, 0.5f, 0f); //주황색
+        // Gizmos.DrawWireSphere(transform.position, 2f);
+        // Gizmos.color = Color.yellow;
+        // Gizmos.DrawWireSphere(transform.position, 3f);
+        // Gizmos.color = Color.green;
+        // Gizmos.DrawWireSphere(transform.position, 4f);
+        // Gizmos.color = Color.blue;
+        // Gizmos.DrawWireSphere(transform.position, 5f);
+        // Gizmos.color = new Color(0.29f, 0f, 0.51f); //남색
+        // Gizmos.DrawWireSphere(transform.position, 6f);
+        // Gizmos.color = new Color(0.58f, 0f, 0.83f); //보라색
+        // Gizmos.DrawWireSphere(transform.position, 7f);
+        //
     }
 #endif
 }
